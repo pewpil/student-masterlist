d2620b22b591b7b32edd8ef2aa1d94514d35fb40
Suikyoo <judeanthony02sayson@gmail.com>
2026-01-12 16:59:46 +0800

    added database sql functions

---
+++ src/lib/db.ts
@@ -0,0 +1,228 @@
+
+"use server"
+
+import dotenv from "dotenv"
+import { Client } from "pg"
+import { Classroom, User } from "./types";
+
+const client = new Client({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DB_SRC,
+  database: process.env.DB_NAME,
+});
+await client.connect();
+
+export async function migrateUp() {
+
+  await client.query(`
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      password VARCHAR(255) NOT NULL
+    );
+  `);
+
+  console.log("user table up");
+
+  await client.query(`
+    CREATE TABLE IF NOT EXISTS classes (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    password VARCHAR(255)
+    );
+  `);
+
+  console.log("classes table up");
+
+  await client.query(`
+    CREATE TABLE IF NOT EXISTS enrollments (
+    id SERIAL PRIMARY KEY,
+    user_id SERIAL,
+    class_id SERIAL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+    FOREIGN KEY (class_id) REFERENCES classes (id)
+    );
+  `);
+
+  console.log("classes table up");
+
+}
+
+export async function migrateDown() {
+  await client.query(`
+DROP TABLE IF EXISTS users, classes, enrollments;
+`);
+  console.log("users, classes, enrollments down");
+}
+
+//user functions
+
+export async function getUsers(): Promise<User[]> {
+  const query = {
+    name: "get-users",
+    text: `
+      FROM users
+      SELECT *;
+    `,
+    values: [],
+  };
+  const res = await client.query(query);
+  return res.rows;
+
+}
+export async function getUserFromId(id: number): Promise<User> {
+  const query = {
+    name: "get-user-from-id",
+    text: `
+    FROM users 
+    SELECT *
+    WHERE id = $1;
+    `,
+    values: [id],
+  };
+  const res = await client.query(query);
+  return res.rows[0];
+}
+
+export async function createUser(user: User): Promise<User> {
+  const users = await getUsers();
+
+  const usernames = users.map(u => u.name);
+
+  if (usernames.includes(user.name)) {
+    throw new Error("username already taken");
+  }
+
+  if (user.password.length < 8) {
+    throw new Error("password must be at least 8 characters");
+  }
+
+  //can add other restrictions here
+  //...
+
+  const query = {
+    name: "create-user",
+    text: `
+      INSERT INTO users(name, password)
+      VALUES ($1, $2)
+      RETURNING *
+    `,
+    values: [user.name, user.password],
+    
+  }
+
+  const res = await client.query(query);
+  return res.rows[0];
+
+}
+
+//class functions
+
+export async function getClasses(): Promise<Classroom[]> {
+  const query = {
+    name: "get-classes",
+    text: `
+      FROM classes
+      SELECT *;
+    `,
+    values: [],
+  };
+  const res = await client.query(query);
+  return res.rows;
+
+}
+export async function getClassFromId(id: number): Promise<Classroom> {
+  const query = {
+    name: "get-class-from-id",
+    text: `
+      FROM classes 
+      SELECT *
+      WHERE id = $1;
+    `,
+    values: [id],
+  };
+  const res = await client.query(query);
+  return res.rows[0];
+}
+
+export async function createClass(classroom: Classroom): Promise<Classroom> {
+  const classes = await getClasses();
+
+  const class_names = classes.map(c => c.name);
+
+  if (class_names.includes(classroom.name)) {
+    throw new Error("class name already taken");
+  }
+
+  if (classroom.password && classroom.password.length < 8) {
+    throw new Error("password must be at least 8 characters");
+  }
+
+  //can add other restrictions here
+  //...
+
+  const query = {
+    name: "create-class",
+    text: `
+      INSERT INTO class(name, password)
+      VALUES ($1, $2)
+      RETURNING *
+    `,
+    values: [classroom.name, classroom.password],
+    
+  }
+
+  const res = await client.query(query);
+  return res.rows[0];
+
+}
+
+//enrollments
+
+export async function createEnrollment(user: User, classroom: Classroom): Promise<void> {
+  if (!user.id) {
+    throw new Error("User not valid");
+  }
+
+  if (!classroom.id) {
+    throw new Error("Class not valid");
+  }
+
+  const classes = await getClassesFromUserId(user.id);
+  const class_names = classes.map(c => c.name);
+
+  if (class_names.includes(classroom.name)) {
+    throw new Error("Already enrolled");
+  }
+
+  const query = {
+    name: "create-enrollment",
+    text: `
+      INSERT INTO enrollments (user_id, class_id) 
+      VALUES ($1, $2);
+      RETURNING id;
+    `,
+    values: [user.id, classroom.id]
+  }
+
+  await client.query(query);
+
+}
+
+export async function getClassesFromUserId(id: number): Promise<Classroom[]> {
+  const query = {
+    name: "get-classes-from-user-id",
+    text: `
+      FROM enrollments
+      SELECT classes.id classes.name classes.password;
+      INNER JOIN users ON enrollments.user_id = users.id
+      INNER JOIN classes ON enrollments.class_id = classes.id
+      WHERE users.id = $1;
+    `,
+    values: [id],
+  };
+  const res = await client.query(query);
+  return res.rows;
+
+}
---
+++ src/lib/types.ts
@@ -0,0 +1,13 @@
+export interface User {
+  id?: number;
+  name: string;
+  password: string;
+};
+
+export interface Classroom { 
+  id?: number;
+  name: string;
+  password?: string;
+}
+
+
